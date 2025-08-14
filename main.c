e5a7d5a51e73670ac6e5dbb3a4618d64b38f8450
ThomasReyel <thomasreyel2231@gmail.com>
2025-08-13 21:08:20 -0300

    feat:add tela menu principal

--- main.c
+++ main.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 
+void telaPrincipal();
+
 int main(void) {
     printf("╔══════════════════════════╗\n");
     printf("║         MENU             ║\n");
@@ -10,6 +12,21 @@ int main(void) {
     printf("║ 4. Sair                  ║\n");
     printf("╚══════════════════════════╝\n");
     printf("Digite sua escolha: ");
-    
+
     return 0;
+}
+
+void telaPrincipal() {
+    printf("╔══════════════════════════╗\n");
+    printf("║      MENU PRINCIPAL      ║\n");
+    printf("╠══════════════════════════╣\n");
+    printf("║ 1. Módulo Assinante      ║\n");
+    printf("║ 2. Módulo Assinaturas    ║\n");
+    printf("║ 3. Módulo Planos         ║\n");
+    printf("║ 4. Módulo Produtos       ║\n");
+    printf("║ 5. Módulo Relatórios     ║\n");
+    printf("║ 6. Sair                  ║\n");
+    printf("╚══════════════════════════╝\n");
+    printf("Digite sua escolha: ");
+
 }
