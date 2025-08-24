b00c1e4812fa8880fde625cfaf62918af52eb5e3
GuiFS05 <guilhermefernandescruzeta@gmail.com>
2025-08-24 16:50:14 +0000

    feat: add telaProduto

---
+++ telaAssinante.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (void){
+    system("clear||cls");
+    printf("╔══════════════════════════╗\n");
+    printf("║         Produto          ║\n");
+    printf("╠══════════════════════════╣\n");
+    printf("║ 1. Nome                  ║\n");
+    printf("║ 2. Tipo                  ║\n");
+    printf("║ 3. Marca                 ║\n");
+    printf("║ 4. Ano de Produção       ║\n");
+    printf("║ 5. Sair                  ║\n");
+    printf("╚══════════════════════════╝\n");
+    printf("Digite sua escolha: \n");
+
