51025ea8cf9b7a3039b08cae7a72ed0d9d29e823
GuiFS05 <guilhermefernandescruzeta@gmail.com>
2025-08-24 16:54:32 +0000

    feat: Add tela assinantes

--- telaAssinante.c
+++ telaAssinante.c
@@ -4,12 +4,12 @@
 int main (void){
     system("clear||cls");
     printf("╔══════════════════════════╗\n");
-    printf("║         Produto          ║\n");
+    printf("║        Assinantes        ║\n");
     printf("╠══════════════════════════╣\n");
     printf("║ 1. Nome                  ║\n");
-    printf("║ 2. Tipo                  ║\n");
-    printf("║ 3. Marca                 ║\n");
-    printf("║ 4. Ano de Produção       ║\n");
+    printf("║ 2. Email                 ║\n");
+    printf("║ 3. CPF                   ║\n");
+    printf("║ 4. Endereço              ║\n");
     printf("║ 5. Sair                  ║\n");
     printf("╚══════════════════════════╝\n");
     printf("Digite sua escolha: \n");
