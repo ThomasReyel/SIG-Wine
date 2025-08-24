c39c0406b6a8c5e0275a6c0e8a34e886a7c1b4e7
GuiFS05 <guilhermefernandescruzeta@gmail.com>
2025-08-24 16:56:46 +0000

    fix: mudança de opções em telaPlano.c

---
+++ telaPlano.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (void){
+    system("clear||cls");
+    printf("╔══════════════════════════╗\n");
+    printf("║          Plano           ║\n");
+    printf("╠══════════════════════════╣\n");
+    printf("║ 1. Nome                  ║\n");
+    printf("║ 2. Perído                ║\n");
+    printf("║ 3. Produtos              ║\n");
+    printf("║ 4. Sair                  ║\n");
+    printf("╚══════════════════════════╝\n");
+    printf("Digite sua escolha: \n");
+
+}
