a705362edb532d1862e93ba56a3527fabdc40ee9
GuiFS05 <guilhermefernandescruzeta@gmail.com>
2025-08-24 16:59:21 +0000

    feat: Add telaAssinatura

---
+++ telaAssinatura.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (void){
+    system("clear||cls");
+    printf("╔══════════════════════════╗\n");
+    printf("║         Produto          ║\n");
+    printf("╠══════════════════════════╣\n");
+    printf("║ 1. ID do Cliente         ║\n");
+    printf("║ 2. ID do Plano           ║\n");
+    printf("║ 3. Data de Assinatura    ║\n");
+    printf("║ 4. Sair                  ║\n");
+    printf("╚══════════════════════════╝\n");
+    printf("Digite sua escolha: \n");
+
+}
